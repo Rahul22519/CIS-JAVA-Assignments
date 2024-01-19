d1e884a6210ac9194f3b5022356a73211ea4b89f
Rahul22519 <venkateswarlu.rahul22519@Gmail.com>
2024-01-19 11:14:34 -0600

    Adding Assignment 3

---
+++ src/Assignment1.java
@@ -0,0 +1,29 @@
+public class Assignment1 {
+    public static void main(String[] args) {
+        String[] Studentname = {"Mani", "Rahul", "Shekar", "Santosh", "Murali", "Ravi", "Ram", "Raj", "Mike", "Wiliam"};
+        String Name = "Rahul";
+        byte Age = 27;
+        short YearsofExp = 10;
+        char Gender = 'M';
+        boolean Handicapped = false;
+        int GraduationYear = 2023;
+        double Gpa = 3.74;
+        long AssetinRuppess = 5000000000L;
+        float Salary = 50000.50f;
+
+        System.out.println("Employee Name:" + Name);
+        System.out.println("Age:" + Age);
+        System.out.println("Gender:" + Gender);
+        System.out.println("Is Employee Handicapped:" + Handicapped);
+        System.out.println("Graduation Year:" + GraduationYear);
+        System.out.println("Masters Gpa:" + Gpa);
+        System.out.println("Total Years of Experience:" + YearsofExp);
+        System.out.println("Salary:" + Salary);
+        System.out.println("Total Assets:" + AssetinRuppess);
+
+        System.out.println("Array First list of Name:" + Studentname[0]);
+        System.out.println("Array Last list of Name:" + Studentname[9]);
+
+    }
+}
+
